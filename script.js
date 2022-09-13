5515d8208152eda13bdeaabb0f67c4be1a88e49c
Ogaga <iyaraogaga69@gmail.com>
2022-09-13 13:23:02 +0100

    Function declaration and function expressions

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice)
 const appleOrangeJuice = fruitProcessor(3, 3)
 console.log(appleOrangeJuice)
+
+// Function Declaration and Function Espression
+
+function calcAge(currentYear, birthYear) {
+    const age = currentYear - birthYear
+    return `You are ${age} years old`
+}
+
+const age1 = calcAge(2022, 1946)
+
+const calcAge2 = function (currentYear, birthYear) {
+    const age = currentYear - birthYear;
+    return `You are ${age} years old`;
+}
+
+const age2 = calcAge2(2022, 2004)
+console.log(age1, age2);
+
