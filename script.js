1015fd81a648a51eed21e9bca55099c5c98bbe4a
Ogaga <iyaraogaga69@gmail.com>
2022-09-13 17:54:21 +0100

    Add checkWinner function

--- script.js
+++ script.js
@@ -27,21 +27,43 @@
 // const age2 = calcAge2(2022, 2004)
 // console.log(age1, age2);
 
-const yearUntilRetirement = (birthYear, currentYear, yearOfEmployment) => {
-    const age = currentYear - birthYear
-    const ageOfRetirement = 60
-    const serviceYear = 25
-    const yearsServed = currentYear - yearOfEmployment
+// const yearUntilRetirement = (birthYear, currentYear, yearOfEmployment) => {
+//     const age = currentYear - birthYear
+//     const ageOfRetirement = 60
+//     const serviceYear = 25
+//     const yearsServed = currentYear - yearOfEmployment
 
-    if (age > ageOfRetirement) {
-        console.log( `You should have been retireed ${age - ageOfRetirement} years ago`)
-    } else if (age === ageOfRetirement || yearsServed === serviceYear) {
-        console.log( `You would retire this year`)
-    } else if (age < ageOfRetirement && yearsServed > serviceYear) {
-        console.log(`You should have retired ${age - yearsServed} years ago`)
-    } else {
-        console.log(`You should retire in ${serviceYear - yearsServed} years time`)
-    }
+//     if (age > ageOfRetirement) {
+//         console.log( `You should have been retireed ${age - ageOfRetirement} years ago`)
+//     } else if (age === ageOfRetirement || yearsServed === serviceYear) {
+//         console.log( `You would retire this year`)
+//     } else if (age < ageOfRetirement && yearsServed > serviceYear) {
+//         console.log(`You should have retired ${age - yearsServed} years ago`)
+//     } else {
+//         console.log(`You should retire in ${serviceYear - yearsServed} years time`)
+//     }
+// }
+
+// yearUntilRetirement(1997, 2040, 2028)
+
+const avgKoalas = (score1, score2, score3) => {
+    const averageScore = (score1 + score2 + score3) / 3
+    return averageScore
 }
 
-yearUntilRetirement(1997, 2040, 2028)
+const avgDolphins = (score1, score2, score3) => {
+  const averageScore = (score1 + score2 + score3) / 3;
+  return averageScore;
+};
+
+const checkWinner = (scoreDolphins, scoreKoalas) => {
+    scoreDolphins = avgDolphins(85, 54, 41)
+    scoreKoalas = avgKoalas(23, 34, 27)
+    if (scoreDolphins >= scoreKoalas * 2) {
+        console.log (`Dolphins win ${scoreDolphins} vs ${scoreKoalas}`)
+    } else if (scoreKoalas >= scoreDolphins * 2) {
+        console.log(`Koalas win ${scoreKoalas} vs ${scoreDolphins}`)
+    }else {console.log(`no one wins`)}
+}
+
+checkWinner()
