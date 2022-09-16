47dac58f13e54f7a3a3bce4d7a8ebb63967b7463
Ogaga <iyaraogaga69@gmail.com>
2022-09-16 13:17:22 +0100

    Add refresh functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,50 +1,57 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent)
-// document.querySelector('.message').textContent = '🎉 Correct Number!'
-// console.log(document.querySelector('.message').textContent);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
-// document.querySelector('.guess').value = 56
+let score = 20;
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1)
+let highScore = 0;
 
-let score = 20
-
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('body').classList.remove('bg');
+  document.querySelector('.number').classList.remove('wide');
+  document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess)
+  const guess = Number(document.querySelector('.guess').value);
 
-    if (!guess) {
-      document.querySelector('.message').textContent = '⛔ No Number ❗';
-    }else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Correct Number !';
-        document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('body').classList.add('bg')
-        document.querySelector('.number').classList.add('wide')
-    } else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent =
-                'Too High 📈';
-            score--
-            document.querySelector('.score').textContent = score
-        } else {
-            document.querySelector('.message').textContent =
-                '💥 Game over';
-            document.querySelector('.score').textContent = 0;
-            document.querySelector('body').classList.add('bg2')
-        }
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent =
-                'Too Low 📉';
-            score--
-            document.querySelector('.score').textContent = score
-        } else {
-            document.querySelector('.message').textContent =
-                '💥 Game over';
-            document.querySelector('.score').textContent = 0;
-            document.querySelector('body').classList.add('bg2');
-        }
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number ❗';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number !';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').classList.add('bg');
+    document.querySelector('.number').classList.add('wide');
+
+    if (score > highScore) {
+      highScore = score;
+
+      document.querySelector('.highscore').textContent = highScore;
     }
-})
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 Game over';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').classList.add('bg2');
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 Game over';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').classList.add('bg2');
+    }
+  }
+});
