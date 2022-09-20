2f2acbbe0606d798d8460c2838770aaffdc93c55
Ogaga <iyaraogaga69@gmail.com>
2022-09-20 13:24:38 +0100

    Add correct variable names

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,27 +1,8 @@
 'use strict';
 
 const modal = document.querySelectorAll('.show-modal')
-console.log(modal)
-
-const display = document.querySelector('.modal')
 
 const closeModal = document.querySelector('.close-modal')
 
 const overLay = document.querySelector('.overlay')
-
-function show () {
-  display.classList.add('hidden');
-  overLay.classList.add('hidden');
-};
-
-for (let i = 0; i < modal.length; i++){
-    modal[i].addEventListener('click', function () {
-        display.classList.remove('hidden')
-        overLay.classList.remove('hidden')
-    })
-}
-
-closeModal.addEventListener('click', show)
-
-overLay.addEventListener('click', show);
 
