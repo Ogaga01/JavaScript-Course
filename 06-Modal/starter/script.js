bd4b61a70dcc4fa91d690f05713397c997d16097
Ogaga <iyaraogaga69@gmail.com>
2022-09-20 13:33:43 +0100

    Add click events and refactor code

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,8 +1,26 @@
 'use strict';
 
-const modal = document.querySelectorAll('.show-modal')
+const btnModal = document.querySelectorAll('.show-modal')
 
-const closeModal = document.querySelector('.close-modal')
+const btnClose = document.querySelector('.close-modal')
 
 const overLay = document.querySelector('.overlay')
 
+const modal = document.querySelector('.modal')
+
+const openModal = function () {
+    modal.classList.remove('hidden')
+    overLay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overLay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnModal.length; i++){
+    btnModal[i].addEventListener('click',openModal )
+}
+
+btnClose.addEventListener('click', closeModal)
+overLay.addEventListener('click', closeModal)
