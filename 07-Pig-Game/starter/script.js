81401ad0684f987a9d6a2fd6a13ea5bff64f0314
Ogaga <iyaraogaga69@gmail.com>
2022-09-21 12:59:47 +0100

    Add switch player functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,29 +1,42 @@
 'use strict';
 
-let playerOneScore = document.getElementById('score--0')
+let playerOneScore = document.getElementById('score--0');
 let playerTwoScore = document.getElementById('score--1');
 let playerOneCurrentScore = document.getElementById('current--0');
 let playerTwoCurrentScore = document.getElementById('current--1');
-const rollDice = document.querySelector('.btn--roll')
-let dice = document.querySelector('.dice')
-let currentScore = 0
+const rollDice = document.querySelector('.btn--roll');
+let dice = document.querySelector('.dice');
+let currentScore = 0;
 
-playerOneScore.textContent = 0
-playerTwoScore.textContent = 0
-dice.classList.add('hidden')
+playerOneScore.textContent = 0;
+playerTwoScore.textContent = 0;
+dice.classList.add('hidden');
+let activePlayer = 0;
 
 rollDice.addEventListener('click', function () {
-    const random = Math.trunc(Math.random() * 6 + 1);
-    console.log(random)
+  const random = Math.trunc(Math.random() * 6 + 1);
+  console.log(random);
 
-    dice.classList.remove('hidden')
-    dice.src = `dice-${random}.png`
+  dice.classList.remove('hidden');
+  dice.src = `dice-${random}.png`;
 
-    if (random !== 1) {
-        currentScore += random
-        playerOneCurrentScore.textContent = currentScore
-    } else {
-        currentScore = 0
-        playerOneCurrentScore.textContent = currentScore;
-    }
-})
+  if (random !== 1) {
+    currentScore += random;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--active');
+
+    currentScore = 0;
+  }
+});
+
+
