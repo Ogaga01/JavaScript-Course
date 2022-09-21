9d4c714836201dcc6be732bca95496b58a7dcf7d
Ogaga <iyaraogaga69@gmail.com>
2022-09-21 13:58:24 +0100

    Add hold event

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,12 +6,30 @@ let playerOneCurrentScore = document.getElementById('current--0');
 let playerTwoCurrentScore = document.getElementById('current--1');
 const rollDice = document.querySelector('.btn--roll');
 let dice = document.querySelector('.dice');
+const hold = document.querySelector('.btn--hold')
+const newGame = document.querySelector('.btn--new')
+
+const scores = [0, 0]
+
 let currentScore = 0;
+let activePlayer = 0;
 
 playerOneScore.textContent = 0;
 playerTwoScore.textContent = 0;
 dice.classList.add('hidden');
-let activePlayer = 0;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.toggle('player--active');
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.toggle('player--active');
+    currentScore = 0;
+}
 
 rollDice.addEventListener('click', function () {
   const random = Math.trunc(Math.random() * 6 + 1);
@@ -25,18 +43,25 @@ rollDice.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
 
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--active');
-
-    currentScore = 0;
+    switchPlayer()
   }
 });
+
+hold.addEventListener('click', function () {
+    scores[activePlayer] += currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+
+    if (scores[activePlayer] >= 100) {
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        dice.classList.add('hidden')
+        rollDice.classList.add('hidden')
+        hold.classList.add('hidden')
+    } else {
+        switchPlayer();
+    }
+})
 
 
