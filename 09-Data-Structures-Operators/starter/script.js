02c87815510436b10b39bc3a708f31cef355bdd1
Ogaga <iyaraogaga69@gmail.com>
2022-09-23 12:51:40 +0100

    Add task 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -78,6 +78,9 @@ const game = {
 const [player1, player2] = game.players
 console.log(player1, player2) 
 
+const [gk, ...fieldPlayers] = player1
+console.log(gk, fieldPlayers)
+
 
 // const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]
 // console.log(menu)
