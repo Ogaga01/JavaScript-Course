4082be698804c48f3c8e5465209c1ef0adedd255
Ogaga <iyaraogaga69@gmail.com>
2022-09-23 12:48:02 +0100

    Add task 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -32,8 +32,55 @@ const restaurant = {
   },
 };
 
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]
-console.log(menu)
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Task 1
+const [player1, player2] = game.players
+console.log(player1, player2) 
+
+
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]
+// console.log(menu)
 
 // const { name: bukka, openingHours, orderMenu } = restaurant
 // console.log(bukka, openingHours, orderMenu)
