1288a4e63ef05bf77bf45f85dd8d4d3d5c7226ef
Ogaga <iyaraogaga69@gmail.com>
2022-09-26 12:23:21 +0100

    Challenge 3 Task 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -155,4 +155,22 @@ const game = {
 // const [c, d, e] = restaurant.categories
 // console.log(c, d, e)
 
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
 
+// CC 3
+//Task 1
+const events = new Set(gameEvents.values())
+
+console.log(events)
