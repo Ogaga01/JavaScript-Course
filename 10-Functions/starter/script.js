4de793c45560ca5b1171ccd417f68196f2e70c30
Ogaga <iyaraogaga69@gmail.com>
2022-09-28 11:38:27 +0100

    Coding challenge

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -49,47 +49,83 @@
 
 // greeting2('hey')('mama');
 
-const airPeace = {
-    airline: 'Air Peace',
-    iataCode: 'AP',
-    bookings: []
-}
+// const airPeace = {
+//     airline: 'Air Peace',
+//     iataCode: 'AP',
+//     bookings: []
+// }
 
-const arikAir = {
-    airline: 'Arik Air',
-    iataCode: 'AA',
-    bookings: []
-}
+// const arikAir = {
+//     airline: 'Arik Air',
+//     iataCode: 'AA',
+//     bookings: []
+// }
 
-function book(flightCode, name) {
-    console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightCode}`)
-    this.bookings.push({flight: `${this.iataCode}${flightCode}`, name})
-}
+// function book(flightCode, name) {
+//     console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightCode}`)
+//     this.bookings.push({flight: `${this.iataCode}${flightCode}`, name})
+// }
 
-book.call(arikAir, 625, 'John')
-console.log(arikAir.bookings)
+// book.call(arikAir, 625, 'John')
+// console.log(arikAir.bookings)
 
-// Bind Method
-const bindAP = book.bind(airPeace)
-bindAP(045, 'Lukewarm Waters')
+// // Bind Method
+// const bindAP = book.bind(airPeace)
+// bindAP(045, 'Lukewarm Waters')
 
-airPeace.planes = 300
-arikAir.planes = 50
+// airPeace.planes = 300
+// arikAir.planes = 50
 
-function buyPlanes() {
-    this.planes++
-    console.log(this)
-    console.log(this.planes)
-}
+// function buyPlanes() {
+//     this.planes++
+//     console.log(this)
+//     console.log(this.planes)
+// }
 
-document.querySelector('.buy').addEventListener('click', buyPlanes.bind(arikAir))
+// document.querySelector('.buy').addEventListener('click', buyPlanes.bind(arikAir))
 
-function addTax(rate) {
-    return function (value) {
-        value += rate * value
-        return value
-    }
-}
+// function addTax(rate) {
+//     return function (value) {
+//         value += rate * value
+//         return value
+//     }
+// }
 
-console.log(addTax(.20)(500))
+// console.log(addTax(.20)(500))
+
+// const poll = {
+// question: "What is your favourite programming language?",
+// options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+// // This generates [0, 0, 0, 0]. More in the next section!
+// answers: new Array(4).fill(0),
+// };
+
+// function displayResults(type = 'array') {
+//     if (type === 'array') {
+//         console.log(this.answers)
+//     } else if (type === 'string') {
+//         console.log(`Poll results are: ${this.answers.join(', ')}`);
+//     }
+// }
+
+
+// const displayBind = displayResults.bind(poll)
+
+// function registerNewAnswer() {
+//     const answer = Number(
+//       window.prompt(
+//         `${this.question}\n ${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+//     if (isNaN(answer) || answer > this.options.length - 1 || answer < 0) {
+//        window.alert(`incorrect answer`)
+//     } else {
+//         this.answers[answer]++
+//     }
+//     displayBind('string')
+// }
+
+// const pollFunc = registerNewAnswer.bind(poll);
+
+// document.querySelector('.poll').addEventListener('click', pollFunc)
 
