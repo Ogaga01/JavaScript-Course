2efa0c0147f554e357da12571e216630b597d0f3
Ogaga <iyaraogaga69@gmail.com>
2022-10-06 11:16:46 +0100

    Add smooth scrolling to nav links

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,13 +1,18 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const header = document.querySelector('.header');
+const message = document.createElement('div');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const navLink = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelector('.nav__links')
 
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault()
   modal.classList.remove('hidden');
@@ -32,9 +37,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const header = document.querySelector('.header')
-
-const message = document.createElement('div')
+// Element Creation and Deleteing
 message.classList.add('cookie-message')
 message.innerHTML = `We use cookies to improve app functionalities. 
 <button class='btn btn--close-cookie'>
@@ -48,11 +51,9 @@ btnCloseCookie.addEventListener('click', () => {
   message.parentElement.removeChild(message)
 })
 
-const btnScrollTo = document.querySelector('.btn--scroll-to')
-const section1 = document.querySelector('#section--1')
-
+//Smooth Scrolling
 btnScrollTo.addEventListener('click', () => {
-  const s1cords = section1.getBoundingClientRect()
+  // const s1cords = section1.getBoundingClientRect()
   // console.log(s1cords)
   // window.scrollTo({
   //   left: s1cords.left + window.pageXOffset,
@@ -62,3 +63,20 @@ btnScrollTo.addEventListener('click', () => {
 
   section1.scrollIntoView({behavior: "smooth"})
 })
+
+navLink.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  })
+})
+
+// navLinks.addEventListener('click', (e) => {
+//   e.preventDefault()
+  
+//   if (e.target.classList.contains('nav__link')) {
+//     const id = e.target.getAttribute('href')
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+//   }
+// })
