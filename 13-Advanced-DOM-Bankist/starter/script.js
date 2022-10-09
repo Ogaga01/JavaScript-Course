e275147f71d3e5296a887617cf79f8175f3812ee
Ogaga <iyaraogaga69@gmail.com>
2022-10-09 12:05:28 +0100

    Add intersection observer to nav

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -123,3 +123,23 @@ function handlenav(e) {
 
 nav.addEventListener('mouseover', handlenav)
 nav.addEventListener('mouseout', handlenav);
+
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = (entries) => {
+  const [entry] = entries
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky')
+  } else {
+    nav.classList.remove('sticky')
+  }
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+})
+
+headerObserver.observe(header)
