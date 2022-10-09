68e6f8a0b7602a39fb2d9286adbd535dd536c92e
Ogaga <iyaraogaga69@gmail.com>
2022-10-09 13:13:44 +0100

    Add intersection observer to sections

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -164,3 +164,26 @@ sections.forEach(section => {
   section.classList.add('section--hidden');
   sectionObserver.observe(section);
 });
+
+const loadImg = (entries, observer) => {
+  const [entry] = entries
+
+  if (!entry.isIntersecting) return
+
+  entry.target.src = entry.target.dataset.src
+
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img')
+  })
+
+  observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0
+})
+
+imgTargets.forEach((img) => {
+  imgObserver.observe(img)
+})
