14f7149348edca64f8675f5208044080593fd303
Ogaga <iyaraogaga69@gmail.com>
2022-10-10 12:03:21 +0100

    Coding challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -16,3 +16,25 @@ Person.prototype.calcAge = function () {
 }
 
 console.log(gee.calcAge(), nick.calcAge)
+
+const Car = function (make, speed) {
+    this.make = make
+    this.speed = speed
+}
+
+Car.prototype.accelerate = function (){
+    this.speed += 10
+    console.log(this.speed)
+}
+
+Car.prototype.brake = function () {
+    this.speed -= 5
+    console.log(this.speed)
+}
+
+const camaro = new Car('Chevrolet', 360)
+camaro.accelerate()
+camaro.brake()
+
+const car1 = new Car('BMW', 120)
+const car2 = new Car('Mercedes', 95)
