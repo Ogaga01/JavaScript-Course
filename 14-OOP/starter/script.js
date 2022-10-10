444556b777e63d2819e6aab3c499fee642cd4e39
Ogaga <iyaraogaga69@gmail.com>
2022-10-10 14:31:03 +0100

    coding challenge3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -118,3 +118,22 @@ Student.prototype = Object.create(Person.prototype)
 const mike = new Student('Mike', 'Scott', 2007, 'Geography')
 console.log(mike.calcAge());
 
+// Coding Challenge 3
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed)
+    this.charge = charge
+}
+EV.prototype = Object.create(Car.prototype)
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo
+}
+EV.prototype.accelerate = function () {
+    this.speed += 20
+    this.charge -= 1
+    console.log(`${this.make} going at ${this.speed}Km/h with a charge of ${this.charge}%`)
+}
+const tesla = new EV('Tesla', 120, 23)
+tesla.accelerate()
+tesla.brake()
+tesla.chargeBattery(90)
+console.log(tesla)
