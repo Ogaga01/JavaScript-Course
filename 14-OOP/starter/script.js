88a66d81d841d88ef6ca80aa21cc1258a8a60d5c
Ogaga <iyaraogaga69@gmail.com>
2022-10-10 11:42:08 +0100

    Create first constructor

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const Person = function (firstname, lastname, birthyear) {
+    this.firstname = firstname
+    this.lastname = lastname
+    this.birthyear = birthyear
+}
+
+const gee = new Person('Ogaga', 'Iyara', 1928)
+const nick = new Person('Nicklaus', 'Mikelson', 1000)
+console.log(gee, nick)
+
+Person.prototype.calcAge = function () {
+    const currentYear = new Date().getFullYear()
+    return currentYear - this.birthyear
+}
+
+console.log(gee.calcAge(), nick.calcAge)
