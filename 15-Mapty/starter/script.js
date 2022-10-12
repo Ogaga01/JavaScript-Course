f41e08b0a467c89885e21c74f840d05c6a097e17
Ogaga <iyaraogaga69@gmail.com>
2022-10-12 08:31:00 +0100

    Add popup data to map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -25,11 +25,24 @@ if (navigator.geolocation) {
           attribution:
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(map);
-
-        L.marker(coords)
-          .addTo(map)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-          .openPopup();
+        
+        map.on('click', (mapEvent) => {
+            console.log(mapEvent)
+            const { lat, lng } = mapEvent.latlng
+            
+            L.marker([lat, lng])
+              .addTo(map)
+              .bindPopup(
+                L.popup({
+                  maxWidth: 250,
+                  minWidth: 100,
+                  autoClose: false,
+                  closeOnClick: false,
+                  className: 'running-popup',
+                })
+              ).setPopupContent('Workout')
+              .openPopup();
+        })
     },
     () => {
         alert('Problem loading map')
