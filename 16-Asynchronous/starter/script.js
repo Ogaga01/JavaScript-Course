d29fcb1db6871b89be54888a68e9767cdf50772a
Ogaga <iyaraogaga69@gmail.com>
2022-10-19 13:12:49 +0100

    Asynchronous JavaScript

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,81 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+// 'https://restcountries.com/v2/'
+
 ///////////////////////////////////////
+// const getCountryData = (country) => {
+//     // const request = new XMLHttpRequest()
+//     // request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//     // request.send()
+
+//     request.addEventListener('load', () => {
+//         // console.log(JSON.parse(request.responseText))
+//         const [data] = JSON.parse(request.responseText)
+//         // console.log(data)
+
+    //     const html = `
+    //     <article class="country">
+    //       <img class="country__img" src="${data.flags.svg}" />
+    //       <div class="country__data">
+    //         <h3 class="country__name">${data.name}</h3>
+    //         <h4 class="country__region">${data.region}</h4>
+    //         <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+    //         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    //         <p class="country__row"><span>💰</span>${data.currencies[0].name} ${data.currencies[0].symbol}</p>
+    //       </div>
+    //     </article>
+    // `;
+    //     countriesContainer.insertAdjacentHTML('beforeend', html)
+    //     countriesContainer.style.opacity = 1
+//     })
+// }
+
+// getCountryData('nigeria')
+// getCountryData('uae')
+// getCountryData('usa')
+// getCountryData('australia')
+
+const renderCountry = (data, classname = '') => {
+            const html = `
+        <article class="country ${classname}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages?.[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies?.[0].name} ${
+              data.currencies?.[0].symbol
+            }</p>
+          </div>
+        </article>
+    `;
+            countriesContainer.insertAdjacentHTML('beforeend', html);
+            countriesContainer.style.opacity = 1;
+            
+}
+
+const getCountryData = (country) => {
+    fetch(`https://restcountries.com/v2/name/${country}`).then((response) => {
+        return response.json()
+    }).then((data) => {
+        const [country] = data
+        renderCountry(country)
+
+        const neighbour = country.borders?.[0]
+        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+        
+    }).then((response) => {
+        return response.json()
+    }).then((data2) => {
+        renderCountry(data2, 'neighbour')
+    });
+}
+
+getCountryData('nigeria')
+getCountryData('uae')
+getCountryData('usa')
+getCountryData('china')
